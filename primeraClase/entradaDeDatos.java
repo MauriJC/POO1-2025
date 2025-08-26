2cf77d23c386270234e6957c3814ab3dc69f77c0
MauriJC <xaviermauricio33@gmail.com>
2025-08-26 20:43:36 -0300

    feat: add first lesson

--- primeraClase/entradaDeDatos.java
+++ primeraClase/entradaDeDatos.java
@@ -1,11 +1,12 @@
 package primeraClase;
+
 import java.util.Scanner;
 
 public class entradaDeDatos {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Ingrese su nombre: ");
-        String nombre = sc.nextLine();
+        String nombre = sc.next();
         System.out.println("Hola " + nombre + "!");
     }
 }
