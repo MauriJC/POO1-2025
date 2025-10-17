d8e6b70bbf7147ea9505b1196bd933ba649ae748
MauriJC <xaviermauricio33@gmail.com>
2025-10-16 22:43:49 -0300

    feat: add Producto class with encapsulation and methods for setting values and displaying information

--- src/_05_constructores/Main.java
+++ src/_05_constructores/Main.java
@@ -2,7 +2,13 @@ package _05_constructores;
 
 public class Main {
     public static void main(String[] args) {
-        Persona p1 = new Persona("Ana", 20);
-        p1.mostrarInfo(); // Nombre: Ana, Edad: 20
+        // Persona p1 = new Persona("Ana", 20);
+        // p1.mostrarInfo(); // Nombre: Ana, Edad: 20
+
+        Producto producto = new Producto("Camiseta", 19.99);
+        Producto prod2 = new Producto("Remera", 0);
+
+        producto.mostrarInfo(); // Nombre del producto: Camiseta, Precio: 19.99
+        prod2.mostrarInfo(); // Nombre del producto: Remera, Precio: 1
     }
 }
---
+++ src/_05_constructores/Producto.java
@@ -0,0 +1,29 @@
+package _05_constructores;
+
+public class Producto {
+    private String nombre;
+    private double precio;
+
+    public Producto(String nombre, double precio) {
+        // this.nombre = nombre; //<= Rompe encapsulamiento
+        // this.precio = precio;
+        setNombre(nombre);
+        setPrecio(precio);
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(double precio) {
+        if (precio > 0) {
+            this.precio = precio;
+        } else {
+            this.precio = 1.0;
+        }
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Nombre del producto: " + nombre + ", Precio: " + precio);
+    }
+}
